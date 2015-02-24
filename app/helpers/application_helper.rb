3b8d6614faa2dcb54d4c1891ccc507d4a0dccd9c
sjones1 <ipadfan.jones@googlemail.com>
2015-02-24 22:23:25 +0000

    commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title(page_title = '')
+    base_title = "Botswana 2016"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
