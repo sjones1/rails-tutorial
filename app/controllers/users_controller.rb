fff0609a23429966688ef49a6d84604fc461988a
sjones1 <ipadfan.jones@googlemail.com>
2015-03-01 16:11:09 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
    @user = User.new(user_params)
    if @user.save
+     flash[:success] = "Welcome to ExploreBotswana"
      render 'show'
    else
      render 'new'
