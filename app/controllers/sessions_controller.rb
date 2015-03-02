b9622feda14e14599da3de6af7954f359482199c
sjones1 <ipadfan.jones@googlemail.com>
2015-03-02 16:52:03 +0000

    fix but no puma

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-    render 'new'
     else    
     flash.now[:danger] = 'Invalid email and/or password'
     render 'new'
