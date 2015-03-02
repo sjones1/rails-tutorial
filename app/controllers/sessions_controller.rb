2ea9760b0dadf7de9380e9ac4e0347633980383a
sjones1 <ipadfan.jones@googlemail.com>
2015-03-02 23:00:22 +0000

    2nd march

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
+      remember user
+      redirect_to user
     else    
     flash.now[:danger] = 'Invalid email and/or password'
     render 'new'
@@ -12,5 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
+
 end
